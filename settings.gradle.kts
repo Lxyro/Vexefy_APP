619596702f296a03b898c580fe695698935a9894
Yiğit Can Yılmaz <yigitcanyilmaz2733@gmail.com>
2026-03-01 15:19:35 +0300

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,2 +1,17 @@
-rootProject.name="VeXeFy"
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        google()
+        mavenCentral()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "VeXeFy"
 include(":app")
