86bfd2e71a233c9a3ce243fa15edb08b7b6dd7e9
Yiğit Can Yılmaz <yigitcanyilmaz2733@gmail.com>
2026-03-01 15:22:42 +0300

    Update and rename module_build.gradle.kts to build.gradle.kts

---
+++ app/build.gradle.kts
@@ -0,0 +1,35 @@
+// app/build.gradle.kts
+plugins {
+    id("com.android.application") version "8.1.1"
+    id("org.jetbrains.kotlin.android") version "1.8.21"
+}
+
+android {
+    compileSdk = 34
+    namespace = "vexefy.live.nofication.android.com"
+
+    defaultConfig {
+        applicationId = "vexefy.live.nofication.android.com"
+        minSdk = 23
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+    }
+
+    buildTypes {
+        getByName("debug") { }
+        getByName("release") {
+            isMinifyEnabled = false
+        }
+    }
+}
+
+repositories {
+    google()
+    mavenCentral()
+}
+
+dependencies {
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib:1.8.21")
+}
